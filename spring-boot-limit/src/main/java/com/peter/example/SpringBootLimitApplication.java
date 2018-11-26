42b6d0f3feb67e95b779fbde6987be4463bc10be
panjianlong13 <876060733@qq.com>
2018-11-26 14:22:49 +0800

    Add Spring Boot Limit

---
+++ spring-boot-limit/src/main/java/com/peter/example/SpringBootLimitApplication.java
@@ -0,0 +1,12 @@
+package com.peter.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLimitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLimitApplication.class, args);
+	}
+}
