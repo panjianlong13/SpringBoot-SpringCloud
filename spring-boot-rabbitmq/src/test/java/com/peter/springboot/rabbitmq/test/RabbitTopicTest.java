c017c1cc40757f735388c6fb98fe98024155397a
panjianlong13 <876060733@qq.com>
2018-12-05 14:42:19 +0800

    Add RabbitMQ

---
+++ spring-boot-rabbitmq/src/test/java/com/peter/springboot/rabbitmq/test/RabbitTopicTest.java
@@ -0,0 +1,75 @@
+package com.peter.springboot.rabbitmq.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.peter.springboot.rabbitmq.run.Startup;
+
+/**
+ * 描述: 配置转发消息模式队列
+ *
+ * @author: yanpenglei
+ * @create: 2017/10/25 1:20
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Startup.class)
+public class RabbitTopicTest {
+
+    @Autowired
+    private AmqpTemplate rabbitTemplate;
+
+    @Test
+    public void sendMessageTest() {
+
+        String context = "此消息在，配置转发消息模式队列下， 有 TopicReceiver1 TopicReceiver2 TopicReceiver3 可以收到";
+
+        String routeKey = "topic.message";
+
+        String exchange = "topicExchange";
+
+        context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
+
+        System.out.println("sendMessageTest : " + context);
+
+        this.rabbitTemplate.convertAndSend(exchange, routeKey, context);
+    }
+
+    @Test
+    public void sendMessagesTest() {
+
+
+        String context = "此消息在，配置转发消息模式队列下，有  TopicReceiver2 TopicReceiver3 可以收到";
+
+        String routeKey = "topic.message.s";
+
+        String exchange = "topicExchange";
+
+        context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
+
+        System.out.println("sendMessagesTest : " + context);
+
+        this.rabbitTemplate.convertAndSend(exchange, routeKey, context);
+    }
+
+    @Test
+    public void sendYmqTest() {
+
+        String context = "此消息在，配置转发消息模式队列下，有 TopicReceiver3 可以收到";
+
+        String routeKey = "topic.ymq";
+
+        String exchange = "topicExchange";
+
+        context = "context:" + exchange + ",routeKey:" + routeKey + ",context:" + context;
+
+        System.out.println("sendYmqTest : " + context);
+
+        this.rabbitTemplate.convertAndSend(exchange, routeKey, context);
+    }
+
+
+}
