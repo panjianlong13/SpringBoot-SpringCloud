5efc6ba5d44056ee45314952eb9cc7b8717bb8cb
panjianlong13 <876060733@qq.com>
2018-12-05 13:33:30 +0800

    Add Rocket

---
+++ spring-boot-rocketmq/src/main/java/com/peter/springboot/rocketmq/SpringBootRocketmqApplication.java
@@ -0,0 +1,12 @@
+package com.peter.springboot.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRocketmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRocketmqApplication.class, args);
+	}
+}
