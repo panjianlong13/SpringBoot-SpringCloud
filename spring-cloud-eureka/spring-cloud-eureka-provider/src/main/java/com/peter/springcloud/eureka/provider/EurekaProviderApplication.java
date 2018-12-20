6a76d27e7e05552cd088eb5cbdc36af627bacd2f
panjianlong13 <876060733@qq.com>
2018-12-20 13:25:08 +0800

    Add Eureka Project

---
+++ spring-cloud-eureka/spring-cloud-eureka-provider/src/main/java/com/peter/springcloud/eureka/provider/EurekaProviderApplication.java
@@ -0,0 +1,23 @@
+package com.peter.springcloud.eureka.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaProviderApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world! Peter";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaProviderApplication.class, args);
+	}
+
+}
