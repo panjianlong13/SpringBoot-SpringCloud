7110ff2de833360ba51b2980b6fb4e920e43d466
panjianlong13 <876060733@qq.com>
2019-01-02 14:08:23 +0800

    Recreate git config project

--- spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/peter/springcloud/config/client/ConfigClientApplication.java
+++ spring-cloud-config-git/spring-cloud-config-client/src/main/java/com/peter/springcloud/config/client/ConfigClientApplication.java
@@ -1,4 +1,4 @@
-package io.ymq.example.config.client;
+package com.peter.springcloud.config.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
