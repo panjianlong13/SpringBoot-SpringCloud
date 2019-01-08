23f1673067db8d7aad0f95ae246103c7cda79e11
panjianlong13 <876060733@qq.com>
2019-01-08 13:19:03 +0800

    Add Zuul Project

--- spring-cloud-config-eureka/spring-cloud-eureka-provider-3/src/main/java/io/ymq/example/eureka/provider/EurekaProviderApplication.java
+++ spring-cloud-config-eureka/spring-cloud-eureka-provider-3/src/main/java/io/ymq/example/eureka/provider/EurekaProviderApplication.java
@@ -3,11 +3,12 @@ package io.ymq.example.eureka.provider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
--- spring-cloud-zuul/spring-cloud-eureka-provider-2/src/main/java/com/peter/springcloud/eureka/provider/EurekaProviderApplication.java
+++ spring-cloud-zuul/spring-cloud-eureka-provider-2/src/main/java/com/peter/springcloud/eureka/provider/EurekaProviderApplication.java
@@ -1,31 +1,27 @@
-package io.ymq.example.eureka.provider;
+package com.peter.springcloud.eureka.provider;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RefreshScope
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class EurekaProviderApplication {
 
-    @Value("${content}")
-    String content;
-
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/")
-    public String home() {
-        return "Hello world ,port:" + port+",content="+content;
+    public String Hello() {
+        return "Hello world ,port:" + port;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaProviderApplication.class, args);
     }
+
 }
